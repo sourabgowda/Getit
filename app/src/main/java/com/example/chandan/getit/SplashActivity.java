202909f969f08cb05c5d8ebb9e86c9c34e4d05a7
Chandan <mgsourab@gmail.com>
2017-03-11 23:04:38 +0530

    First Commit

---
+++ app/src/main/java/com/example/chandan/getit/SplashActivity.java
@@ -0,0 +1,32 @@
+package com.example.chandan.getit;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        Thread mythread=new Thread(){
+            @Override
+            public void run(){
+                try {
+                    sleep(5000);
+                    Intent intent=new Intent(getApplicationContext(),LoginActivity.class);
+                    startActivity(intent);
+                    finish();
+
+                }
+                catch(InterruptedException e){
+                   e.printStackTrace();
+                }
+            }
+        };
+        mythread.start();
+
+
+    }
+}
