83a519a8a0c27e5bac2495730dc4b9ed8ff81344
Chandan <mgsourab@gmail.com>
2017-03-20 22:14:42 +0530

    added addstudent and faculty UI

--- app/src/main/java/com/example/chandan/getit/FacultyActivity.java
+++ app/src/main/java/com/example/chandan/getit/FacultyActivity.java
@@ -1,13 +1,27 @@
 package com.example.chandan.getit;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class FacultyActivity extends AppCompatActivity {
+    Button facultyAdd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_faculty);
+        Button facultyAdd=(Button)findViewById(R.id.People);
+
     }
+
+    public void facultyAdd(View view){
+        Intent intent=new Intent(this,SelectAddActivity.class);
+        startActivity(intent);
+
+    }
+
+
 }
--- app/src/main/java/com/example/chandan/getit/LoginActivity.java
+++ app/src/main/java/com/example/chandan/getit/LoginActivity.java
@@ -2,24 +2,17 @@ package com.example.chandan.getit;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
-
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 
 public class LoginActivity extends AppCompatActivity {
     EditText name;
     EditText phone;
     Button login;
     Spinner designation;
-    DatabaseReference myRef= FirebaseDatabase.getInstance().getReference();
+
 
 
     @Override
@@ -33,19 +26,6 @@ public class LoginActivity extends AppCompatActivity {
 
     }
     public void onLoginClick(View view){
-        myRef.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                String value = dataSnapshot.getValue(String.class);
-                Log.d(TAG, "Value is: " + value);
-            }
-
-            @Override
-            public void onCancelled(DatabaseError error) {
-                // Failed to read value
-                Log.w(TAG, "Failed to read value.", error.toException());
-            }
-        });
 
     }
 }
